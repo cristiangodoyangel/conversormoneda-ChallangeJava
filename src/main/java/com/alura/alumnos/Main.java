4e98fdd4fb91b069e241ee19f939fe6399fd7c32
Star <creacciones@gmail.com>
2024-11-17 19:22:38 -0300

    - Se crea clase principal con mensaje de bienvenida al conversor

--- src/main/java/com/alura/alumnos/Main.java
+++ src/main/java/com/alura/alumnos/Main.java
@@ -2,6 +2,7 @@ package com.alura.alumnos;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido al Conversor de Monedas.");
+        // Aquí comenzaremos a construir el menú y la lógica del programa.
     }
 }
